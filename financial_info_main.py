84e460b0ee23dc9ca56936cde7ae27e30444f588
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-04-07 23:32:02 -0600

    DONE WITH EVERYTHING!!!

--- financial_info_main.py
+++ financial_info_main.py
@@ -3,10 +3,11 @@
 from income import income
 from expense import expense
 from display_info import display_csv
+from view_money_time_period import view_money_history
 
 def financial_info_main():
     while True:
-        choice = input('Which one would you like to do?:\n\n1. Add an income to your history\n2. Add an expense to your history\n3. View your money history\n4. Exit this part\n\n(1-4): ')
+        choice = input('Which one would you like to do?:\n\n1. Add an income to your history\n2. Add an expense to your history\n3. View your money history\n4. View money history within a certain number of weeks.\n5. Exit this part\n\n(1-5): ')
 
         if choice == '1':
             income()
@@ -15,6 +16,12 @@ def financial_info_main():
         elif choice == '3':
             display_csv()
         elif choice == '4':
+            try:
+                num_weeks = int(input('How many weeks would you like to check?: '))
+                view_money_history(num_weeks)
+            except ValueError:
+                print("Enter a number next time.")
+        elif choice == '5':
             print('\nGoing back to the main function.')
             break
         else:
--- view_money_time_period.py
+++ view_money_time_period.py
@@ -1,2 +1,36 @@
-# Connor Pavicic, View a specific time period for money logs
+# Connor Pavicic, View a specific time period for money logs.
 
+import csv
+from datetime import datetime, timedelta
+
+def view_money_history(weeks):
+    #Calculate date cutoff
+    today = datetime.today()
+    cutoff_date = today - timedelta(weeks=weeks)
+
+    #Read the csv
+    with open("money_tracking.csv", "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+        print(f"Entries in the last {weeks} week(s):\n")
+
+        found = False
+
+        for row in reader:
+            if not row:
+                continue
+            entry = row[0] #Get the string
+            try:
+                # Extract string date
+                date_str = entry.split(",")[0].replace("Date: ", "").strip()
+                entry_date = datetime.strptime(date_str, "%Y-%m-%d")
+
+                if entry_date >= cutoff_date:
+                    print(entry)
+                    found = True
+            except Exception as e:
+                print("Error with line {entry}")
+                print(f"Error: {e}")
+        
+        if not found:
+            print('No entries found within that time span.')
